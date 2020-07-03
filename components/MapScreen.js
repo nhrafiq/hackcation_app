77a7d2e6e37344f7fe4e10dfe4651a00fca0e63f
Noor <nr5dr@virginia.edu>
2020-07-03 17:51:37 -0400

    added markers

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-export default function MapScreen() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-    </View>
-  );
+const markers = ['Home', 'New York'];
+
+class MapScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          ref={ref => {
+            this.map = ref;
+          }}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          onMapReady={() => {
+            this.map.fitToSuppliedMarkers(markers);
+          }}>
+          <Marker
+            identifier="Home"
+            coordinate={{latitude: 37.78825, longitude: -122.4324}}
+          />
+
+          <Marker
+            identifier="New York"
+            coordinate={{latitude: 40.7128, longitude: -74.006}}
+          />
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -29,3 +48,5 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+
+export default MapScreen;
