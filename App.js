a021741d3ab9c0ba24f24b3fc938bf3725c3004d
Noor <nr5dr@virginia.edu>
2020-07-03 14:19:22 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>welcome to the amazing not race</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
