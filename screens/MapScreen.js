1ca988f146383b98dbe5677f2e725de680cca763
Noor <nr5dr@virginia.edu>
2020-07-04 12:21:49 -0400

    added maps

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,24 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import MapView from "react-native-maps";
+import { View, Text, StyleSheet } from "react-native";
 
 export function MapScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Map Screen</Text>
-      </View>
-    );
-  }
-  
-  
+	return (
+		<View style={styles.container}>
+			<MapView style={styles.mapStyle} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	mapStyle: {
+		width: "100%",
+		height: "100%",
+	},
+});
