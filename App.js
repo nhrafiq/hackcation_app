6b444cbd4b23bef11755dff342abb8f9bd714be5
agarwalmanya <manya18ag@gmail.com>
2020-07-04 21:33:00 +0530

    navigation control

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Pedometer } from 'expo-sensors';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './screens/HomeScreen';
+import { MapScreen } from './screens/MapScreen'; 
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FFF7EB',
+    accent: '#246A73',
+  },
+};
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        App
-      </View>
+      <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+          <Stack.Screen name="Map" component={MapScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      </PaperProvider>
     );
   }
 }
