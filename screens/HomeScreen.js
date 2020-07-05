ec52425ccb97e1fb9a31eef3a01ee27d6ce9a0dd
Noor <nr5dr@virginia.edu>
2020-07-04 22:42:10 -0400

    alignment of cards and titles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,129 +3,134 @@ import { View, StyleSheet } from "react-native";
 import { Title, IconButton, Colors, Card } from "react-native-paper";
 
 const locations = [
-  {
-    key: "Paris",
-    title: "Paris",
-    url: "https://www.gpsmycity.com/img/gd_cover/1144.jpg",
-    tours: [
-      {
-        title: "Le Louvre",
-        image:
-          "https://img.theweek.in/content/dam/week/webworld/feature/society/2016/august/images/Louvre-museum-paris.jpg",
-        url: "https://petitegalerie.louvre.fr/visite-virtuelle/saison5/",
-      },
-      {
-        title: "Eiffel Tower",
-        image:
-          "https://cdn.theculturetrip.com/wp-content/uploads/2018/05/eiffel-tower-3349075_1280-1.jpg",
-        url: "https://www.youtube.com/watch?v=nbD8XYTw23Y",
-      },
-    ],
-  },
-  {
-    key: "Rome",
-    title: "Rome",
-    url:
-      "https://mymodernmet.com/wp/wp-content/uploads/2019/05/colosseum-facts-3.jpg",
-    tours: [
-      {
-        title: "Colosseum",
-        image:
-          "https://www.planetware.com/wpimages/2019/05/italy-rome-colosseum-visiting-highlights-tips-tours.jpg",
-        url: "https://www.youtube.com/watch?v=eJeF7hDB0UA",
-      },
+	{
+		key: "Paris",
+		title: "Paris",
+		url: "https://www.gpsmycity.com/img/gd_cover/1144.jpg",
+		tours: [
+			{
+				title: "Le Louvre",
+				image:
+					"https://img.theweek.in/content/dam/week/webworld/feature/society/2016/august/images/Louvre-museum-paris.jpg",
+				url:
+					"https://petitegalerie.louvre.fr/visite-virtuelle/saison5/",
+			},
+			{
+				title: "Eiffel Tower",
+				image:
+					"https://cdn.theculturetrip.com/wp-content/uploads/2018/05/eiffel-tower-3349075_1280-1.jpg",
+				url: "https://www.youtube.com/watch?v=nbD8XYTw23Y",
+			},
+		],
+	},
+	{
+		key: "Rome",
+		title: "Rome",
+		url:
+			"https://mymodernmet.com/wp/wp-content/uploads/2019/05/colosseum-facts-3.jpg",
+		tours: [
+			{
+				title: "Colosseum",
+				image:
+					"https://www.planetware.com/wpimages/2019/05/italy-rome-colosseum-visiting-highlights-tips-tours.jpg",
+				url: "https://www.youtube.com/watch?v=eJeF7hDB0UA",
+			},
 
-      {
-        title: "Pantheon",
-        image: "https://resize.hswstatic.com/w_907/gif/pantheon-parthenon.jpg",
-        url: "https://www.360cities.net/image/pantheon-rome",
-      },
-    ],
-  },
-  {
-    key: "Cairo",
-    title: "Cairo",
-    url:
-      "https://www.corinthiantravel.co.uk/blog/wp-content/uploads/2016/07/egypt-1024x680.jpg",
-    tours: [
-      {
-        title: "Tomb of Queen Meresankh III",
-        image:
-          "https://lh3.googleusercontent.com/proxy/Qn25iFdMEYCicmXg8kncPefiv_E0BGXViSBXyQ1YfeQOBy0LjvfGNWRWYLWOqY8KSYV7VftuoxOvsfJJie5OPWToCPyH_sdlUidXAdMLlETBIydys4e9bQBpzH0EYBCQ5_H2WZgoVe5XnIhrhPpUrzcr7T9f5g",
-        url: "https://my.matterport.com/show/?m=d42fuVA21To",
-      },
-      {
-        title: "The Great Pyramids of Giza",
-        image:
-          "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555389938/shape/mentalfloss/istock_000020293157_small_0.jpg?itok=l_HzbeIQ",
-        url: "https://www.youtube.com/watch?v=TMzouTzim0o",
-      },
-    ],
-  },
+			{
+				title: "Pantheon",
+				image:
+					"https://resize.hswstatic.com/w_907/gif/pantheon-parthenon.jpg",
+				url: "https://www.360cities.net/image/pantheon-rome",
+			},
+		],
+	},
+	{
+		key: "Cairo",
+		title: "Cairo",
+		url:
+			"https://www.corinthiantravel.co.uk/blog/wp-content/uploads/2016/07/egypt-1024x680.jpg",
+		tours: [
+			{
+				title: "Tomb of Queen Meresankh III",
+				image:
+					"https://lh3.googleusercontent.com/proxy/Qn25iFdMEYCicmXg8kncPefiv_E0BGXViSBXyQ1YfeQOBy0LjvfGNWRWYLWOqY8KSYV7VftuoxOvsfJJie5OPWToCPyH_sdlUidXAdMLlETBIydys4e9bQBpzH0EYBCQ5_H2WZgoVe5XnIhrhPpUrzcr7T9f5g",
+				url: "https://my.matterport.com/show/?m=d42fuVA21To",
+			},
+			{
+				title: "The Great Pyramids of Giza",
+				image:
+					"https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555389938/shape/mentalfloss/istock_000020293157_small_0.jpg?itok=l_HzbeIQ",
+				url: "https://www.youtube.com/watch?v=TMzouTzim0o",
+			},
+		],
+	},
 ];
 
 export function HomeScreen({ navigation }) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Title style={styles.title}>Destinations Unlocked</Title>
-      <View style={styles.cards}>
-        {locations.map((location) => (
-          <Card
-            key={location.key}
-            style={styles.card}
-            onPress={() =>
-              navigation.navigate("Destination", { location: location })
-            }
-          >
-            <Card.Cover source={{ uri: location.url }} />
-            <Card.Title title={location.title} />
-          </Card>
-        ))}
-      </View>
-      <IconButton
-        icon="navigation"
-        color="#246A73"
-        size={40}
-        style={styles.button}
-        onPress={() => navigation.navigate("Map")}
-      />
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				alignItems: "center",
+				justifyContent: "center",
+				flexDirection: "column",
+			}}
+		>
+			<Title style={styles.title}>Destinations Unlocked</Title>
+			<View style={styles.cards}>
+				{locations.map((location) => (
+					<Card
+						key={location.key}
+						style={styles.card}
+						onPress={() =>
+							navigation.navigate("Destination", {
+								location: location,
+							})
+						}
+					>
+						<Card.Cover source={{ uri: location.url }} />
+						<Card.Title title={location.title} />
+					</Card>
+				))}
+			</View>
+			<IconButton
+				icon="navigation"
+				color="#246A73"
+				size={40}
+				style={styles.button}
+				onPress={() => navigation.navigate("Map")}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  cards: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignContent: "flex-start",
-    marginTop: 50,
-  },
-  title: {
-    position: "absolute",
-    top: 10,
-  },
-  button: {
-    position: "absolute",
-    bottom: 10,
-    borderRadius: 70,
-    height: 70,
-    width: 70,
-    borderStyle: "solid",
-    borderWidth: 5,
-    borderColor: "#246A73",
-  },
-  card: {
-    height: "40%",
-    width: "45%",
-    alignSelf: "flex-start",
-    marginRight: "2.5%",
-  },
+	cards: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignContent: "flex-start",
+		marginTop: 50,
+		flexWrap: "wrap",
+	},
+	title: {
+		position: "absolute",
+		top: 10,
+	},
+	button: {
+		position: "absolute",
+		bottom: 10,
+		borderRadius: 70,
+		height: 70,
+		width: 70,
+		borderStyle: "solid",
+		borderWidth: 5,
+		borderColor: "#246A73",
+	},
+	card: {
+		height: "40%",
+		width: "45%",
+		alignSelf: "flex-start",
+		margin: "2.5%",
+	},
 });
